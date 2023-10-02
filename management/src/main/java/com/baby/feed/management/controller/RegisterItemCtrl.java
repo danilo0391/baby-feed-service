940e2a49eb97eaad17d673ec34c9e78418d6cf64
Danilo Pereira <danilo_tw@hotmail.com>
2023-10-02 22:16:18 +0100

    aula 1

---
+++ management/src/main/java/com/baby/feed/management/controller/RegisterItemCtrl.java
@@ -0,0 +1,21 @@
+package com.baby.feed.management.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin("*")
+public class RegisterItemCtrl {
+
+    @RequestMapping("/items")
+    public String[] getAllItems(){
+        return new String[]{"Fraldinha", "Coco", "Fralda", "Xixi"};
+    }
+
+    @RequestMapping("/items/id")
+    public String[] getItemById(){
+        return new String[]{"Fraldinha", "Coco"};
+    }
+
+}
